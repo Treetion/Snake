1438a83bb7e36db6542aa2b8b4d0d2f06e961018
Shu <treetion0619@gmail.com>
2024-08-02 02:04:28 -0700

    Speed up game

--- src/utils/handleTimer.js
+++ src/utils/handleTimer.js
@@ -5,7 +5,7 @@ const handleTimer = (running, dispatch) => {
   if (running) {
     timeoutId = setTimeout(() => {
       dispatch({ type: "increment timer" });
-    }, 1000);
+    }, 500);
   } else {
     dispatch({ type: "reset timer" });
   }
